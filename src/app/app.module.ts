3606df4ccafa0f4e1587225c9d5e7938d97a44de
Alexis <alexis.perez.medina@gmail.com>
2024-09-25 16:07:56 +0200

    add router link and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { TiempoComponent } from './components/tiempo/tiempo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent,
+    TiempoComponent
   ],
   imports: [
     BrowserModule,
